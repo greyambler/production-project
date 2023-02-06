57189ff6a23850696748c701c8cb1ba5a2039ea5
greyambler <greyambler@rambler.ru>
2023-02-06 15:59:40 +0300

    12 Navbar. Шаблоны для разработки.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 
 import "./styles/index.scss";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -13,11 +13,12 @@ const App = () => {
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-
+      {/* <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link> */}
+      <Navbar />
       <AppRouter />
+
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
