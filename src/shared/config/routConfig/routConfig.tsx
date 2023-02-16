ee7f392f1137fcb1951e35ce77be45dcc126203b
greyambler <greyambler@rambler.ru>
2023-02-16 16:38:29 +0300

    28 Сайдбар_Состояния_кнопки

--- src/shared/config/routConfig/routConfig.tsx
+++ src/shared/config/routConfig/routConfig.tsx
@@ -1,19 +1,19 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { NotFoundPage } from 'pages/NotFoundPage';
 import { RouteProps } from 'react-router-dom';
+import { MainPage } from 'pages/MainPage';
+import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
-  MAIN = 'main',
-  ABOUT = 'about',
-  NOT_FOUNT = 'not_foun',
+    MAIN = 'main',
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: 'about',
-
-    [AppRoutes.NOT_FOUNT]: '*',
+    [AppRoutes.ABOUT]: '/about',
+    // последний
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -25,9 +25,8 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
         path: RoutePath.about,
         element: <AboutPage />,
     },
-
-    [AppRoutes.NOT_FOUNT]: {
-        path: RoutePath.not_foun,
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
         element: <NotFoundPage />,
     },
 };
--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,25 +1,27 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button, ThemeButton } from '../Button/Button';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps {
-  className?: string;
+    className?: string;
+    short?: boolean;
 }
 
-export const LangSwitcher = ({ className }: LangSwitcherProps) => {
-    const { t, i18n } = useTranslation();
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation('translation');
 
-    const toggle = () => {
+    const toggle = async () => {
         i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
     };
 
     return (
         <Button
             className={classNames('', {}, [className])}
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             onClick={toggle}
         >
-            {t('Язык')}
+            {t(short ? 'Короткий язык' : 'Язык', { ns: 'translation' })}
         </Button>
     );
 };
