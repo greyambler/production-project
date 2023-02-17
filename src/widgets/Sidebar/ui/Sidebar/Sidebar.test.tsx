583b739bc8ce02dcc29a0ae5c7cdd4bcb8b8cb8f
greyambler <greyambler@rambler.ru>
2023-02-17 16:46:36 +0300

    28 Сайдбар. Состояния кнопки. UI Screenshot test 2

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTranslatin } from 'shared/lib/tests/renderWithTranslatin/renderWithTranslatin';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('with onle first param', () => {
-        renderWithTranslatin(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         screen.debug();
     });
 
     test('test toggle', () => {
-        renderWithTranslatin(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
