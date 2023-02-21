27b0c564bcc1a1f62b32223848614683d831abdb
greyambler <greyambler@rambler.ru>
2023-02-21 15:28:20 +0300

    30 Redux-toolkit. Entity. Тесты на всех уровнях

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,15 @@
+import { Counter } from 'entities/Counter';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
 
-    return <div>{t('О сайте', { ns: 'about' })}</div>;
+    return (
+        <div>
+            {t('О сайте', { ns: 'about' })}
+            <Counter />
+        </div>
+    );
 };
 export default AboutPage;
