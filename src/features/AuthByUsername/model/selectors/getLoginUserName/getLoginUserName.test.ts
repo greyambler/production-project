d795c1498dacc39e5b83337e72127c9cb14ebc9e
greyambler <greyambler@rambler.ru>
2023-02-28 18:11:02 +0300

    36 Тест-ие фичи authByUsername. TestAsyncThunk

---
+++ src/features/AuthByUsername/model/selectors/getLoginUserName/getLoginUserName.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUserName } from './getLoginUserName';
+
+describe('getLoginUserName.test.ts', () => {
+    test('should return username 123123', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: '123123',
+            },
+        };
+        expect(getLoginUserName(state as StateSchema)).toEqual('123123');
+    });
+    test('should work with empty username state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUserName(state as StateSchema)).toEqual('');
+    });
+});
